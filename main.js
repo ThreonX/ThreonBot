f4f6bdecd364a2dcfc1323a2613281a18aaea31f
ThreonX <xbwen2016@gmail.com>
2021-09-24 02:24:27 +1200

    working test reply

--- main.js
+++ main.js
@@ -1,14 +1,36 @@
-// require stuff
-const { Client, Intents } = require('discord.js');
+
+// setup
+const { Client, Intents, Message, MessageEmbed } = require('discord.js');
+const client = new Client({intents : [Intents.FLAGS.GUILD_MESSAGES , Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILDS]});
 const { token } = require('./config.json');
-
-
-// create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS]});
 
 client.once ('ready', () => {
     console.log('Ready');
 });
 
 
+
+
+const prefix = '!';
+
+/**
+ * respons on message create
+ */
+client.on('messageCreate', message => {
+
+    // return if not a ! type command
+    if (!message.content.startsWith(prefix)){
+        return;
+    }
+    // messgae is asdf,asdf
+    const command = message.content.substr(prefix.length).split(/ +/);
+
+ 
+    console.log('messgae is ' + command);
+
+});
+
+
+
+// login
 client.login(token);
